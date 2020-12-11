89b53dacdb36260ee190a3cacadefffd20e5cf3e
David Domínguez López <daviddominguezlopez@gmail.com>
2020-12-11 11:09:52 +0100

    -Añadida compatibilidad con la BBDD MongoDB. -Añadido cliente Feign.

--- src/main/java/com/formacionbdi/microservicios/app/respuestas/service/RespuestaServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/respuestas/service/RespuestaServiceImpl.java
@@ -1,17 +1,26 @@
 package com.formacionbdi.microservicios.app.respuestas.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formacionbdi.microservicios.app.respuestas.clients.ExamenFeignClient;
 import com.formacionbdi.microservicios.app.respuestas.models.entity.Respuesta;
 import com.formacionbdi.microservicios.app.respuestas.models.repository.RespuestaRepository;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Pregunta;
 
 @Service
 public class RespuestaServiceImpl implements RespuestaService {
 
 	@Autowired
 	private RespuestaRepository respuestaRepository;
+
+	@Autowired
+	private ExamenFeignClient examenClient;
 
 	@Override
 	@Transactional
@@ -22,12 +31,30 @@ public class RespuestaServiceImpl implements RespuestaService {
 	@Override
 	@Transactional(readOnly = true)
 	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId) {
-		return respuestaRepository.findRespuestaByAlumnoByExamen(alumnoId, examenId);
+		// Obtenemos el examen
+		Examen examen = examenClient.obtenerExamenPorId(examenId);
+		// Obtenemos las preguntas del examen
+		List<Pregunta> preguntas = examen.getPreguntas();
+		// Obtenemos el id de cada pregunta del examen.
+		List<Long> preguntaIds = preguntas.stream().map(p -> p.getId()).collect(Collectors.toList());
+		// Obtenemos un iterable con las respuestas (para un examen en particula) que ha dado un alumno en concreto.
+		List<Respuesta> respuestas = (List<Respuesta>) respuestaRepository.findRespuestaByAlumnoByPreguntaIds(alumnoId,
+				preguntaIds);
+		// Asignamos cada objeto pregunta a cada objeto respuesta. Debe coincidir pregunta.getId con respuesta.preguntaId
+		respuestas.stream().map(r -> {
+			preguntas.forEach(p -> {
+				if (p.getId() == r.getPreguntaId()) {
+					r.setPregunta(p);
+				}
+			});
+			return r;
+		}).collect(Collectors.toList());
+		return respuestas;
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Iterable<Long> findExamenesIdsConRespuestasByAlumno(Long alumnoId) {
-		return respuestaRepository.findExamenesIdsConRespuestasByAlumno(alumnoId);
+		return null;
 	}
 }
